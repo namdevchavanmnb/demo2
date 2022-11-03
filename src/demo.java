87bed9ce8e442f4f47a63c1690a1b6d3cec9d834
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-03 20:44:55 +0530

    Welcome

---
+++ src/demo.java
@@ -0,0 +1,8 @@
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
